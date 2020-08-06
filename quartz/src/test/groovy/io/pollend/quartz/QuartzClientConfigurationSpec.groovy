fb2028ee85f0669e32e1ea499fa09c245bcc9082
Michael <polli104@mail.chapman.edu>
2020-08-05 17:25:44 -0700

    adjust test

--- quartz/src/test/groovy/io/pollend/quartz/QuartzClientConfigurationSpec.groovy
+++ quartz/src/test/groovy/io/pollend/quartz/QuartzClientConfigurationSpec.groovy
@@ -36,7 +36,7 @@ class QuartzClientConfigurationSpec extends Specification{
             "quartz.clients.two.config-file"    : "classpath:example.properties",
             "quartz.triggers[0].start-now"      : true,
             "quartz.triggers[0].description"    : "description of the trigger",
-            "quartz.triggers[1].target"         : "io.pollend.quartz.jobs.Job1"
+            "quartz.triggers[0].target"         : "io.pollend.quartz.jobs.Job1"
         ])
 
         when:
@@ -46,15 +46,14 @@ class QuartzClientConfigurationSpec extends Specification{
 
 
         then:
-        triggers.size() == 2
+        triggers.size() == 1
 
         Trigger trigger = triggers[0].getTrigger().build()
         Math.abs(trigger.startTime.getTime() - now.getTime()) < 200
         trigger.getDescription() == "description of the trigger"
-        !triggers[0].getTarget().isPresent()
 
-        triggers[1].target.isPresent()
-        triggers[1].target.get().is(Job1.class)
+        triggers[0].target.isPresent()
+        triggers[0].target.get().is(Job1.class)
 
 
     }
